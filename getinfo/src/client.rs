185cdab6935fdb1f471a4f74fb0f400b1a1badf6
costa <costa@hellotalk.com>
2024-02-05 17:45:49 +0800

    feat: tokio

--- getinfo/src/client.rs
+++ getinfo/src/client.rs
@@ -1,38 +1,58 @@
 use std::env;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use tokio_util::codec::{Framed, LengthDelimitedCodec};
+use bytes::Bytes;use futures::{SinkExt, StreamExt};
 #[tokio::main]
 async fn main()-> Result<(), Box<dyn std::error::Error>>{
     let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
     println!("Listening on: {}", addr);
 
-    let mut stream = TcpStream::connect(&addr).await?;
+   
+    // let mut stream = TcpStream::connect(&addr).await?;
 
-    let directive = "gettime";
+    // let directive = "gettime";
     // 发送指令
-    stream.write_all(directive.as_bytes()).await?;
+    // stream.write_all(directive.as_bytes()).await?;
 
-    // 等待tcp server的回复，读取内容 // 这里用动态数组来存储从服务端返回的内容 
-    let mut buf: Vec<u8> = Vec::with_capacity(8128);
+    // // 等待tcp server的回复，读取内容 // 这里用动态数组来存储从服务端返回的内容 
+    // let mut buf: Vec<u8> = Vec::with_capacity(8128);
 
-    // 缓冲区
-    let mut resp = [0u8; 2048];
-    loop {
-        let n = stream.read(&mut resp).await?;
-        buf.extend_from_slice(&resp[0..n]);
-        if n == 0 {
-            panic!("connection closed by peer");
-        }else if  buf.len() >= 28 {
-            // like: "Tue Oct 31 14:56:27 CST 2023" 
-            // buf 已经填充了足够的内容
-            break;
-        }else {
-            continue;
+    // // 缓冲区
+    // let mut resp = [0u8; 2048];
+    // loop {
+    //     let n = stream.read(&mut resp).await?;
+    //     buf.extend_from_slice(&resp[0..n]);
+    //     if n == 0 {
+    //         panic!("connection closed by peer");
+    //     }else if  buf.len() >= 28 {
+    //         // like: "Tue Oct 31 14:56:27 CST 2023" 
+    //         // buf 已经填充了足够的内容
+    //         break;
+    //     }else {
+    //         continue;
+    //     }
+    // }
+
+    // // 打印服务端返回的内容
+    // let timeinfo = String::from_utf8(buf)?;
+    // println!("timeinfo: {}", timeinfo);
+
+
+// v2
+        let  stream = TcpStream::connect(&addr).await?;
+        let mut framed_stream = Framed::new(stream, LengthDelimitedCodec::new());
+        let directive = "gettime";
+        framed_stream.send(Bytes::from(directive)).await?;
+
+        if let Some(msg) = framed_stream.next().await {
+            if (&msg).is_err() {
+                return Err("error from server".into());
+            }
+
+            let timeinfo = String::from_utf8(msg?.to_vec())?;
+            println!("timeinfo: {}", timeinfo);
         }
-    }
 
-    // 打印服务端返回的内容
-    let timeinfo = String::from_utf8(buf)?;
-    println!("timeinfo: {}", timeinfo);
     Ok(())
 }
--- getinfo/src/server.rs
+++ getinfo/src/server.rs
@@ -1,7 +1,11 @@
 use std::env;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 use tokio::process::Command;
+use tokio_util::codec::{Framed, LengthDelimitedCodec};
+use bytes::Bytes;
+use futures::{SinkExt, StreamExt};
+
 #[tokio::main]
 async fn main() ->Result<(), Box<dyn std::error::Error>> {
     let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
@@ -10,39 +14,59 @@ async fn main() ->Result<(), Box<dyn std::error::Error>> {
     // 通过循环接受连接
     loop {
         // 接受连接
-        let (mut socket, _ ) = listening.accept().await?;
+        // let (mut socket, _ ) = listening.accept().await?;
+        let (socket, _ ) = listening.accept().await?;
+
+        // 包裹成一个Frame stream
+        let mut framed_stream = Framed::new(socket,LengthDelimitedCodec::new());
         // 处理连接
         tokio::spawn(async move {
-            // 分配一个新的buffer
-            let mut buf = vec![0; 1024];
-            let mut offset = 0;
-            loop {
-                // 读取数据
-                let n = socket.read(&mut buf[offset..]).await.expect("failed to read data from socket");
-                // 如果读取的数据为0，说明连接已经关闭
-                if n == 0 {
-                    return;
-                }
-                // 打印接收到的数据
-                println!("offset: {offset}, n: {n}");
-               let end = offset +n;
-                // 转换指令为字符串 
-                if let Ok(s) = std::str::from_utf8(&buf[..end]) {
-                    println!("Received: {}", s);
+            // // 分配一个新的buffer
+            // let mut buf = vec![0; 1024];
+            // let mut offset = 0;
+            // loop {
+            //     // 读取数据
+            //     let n = socket.read(&mut buf[offset..]).await.expect("failed to read data from socket");
+            //     // 如果读取的数据为0，说明连接已经关闭
+            //     if n == 0 {
+            //         return;
+            //     }
+            //     // 打印接收到的数据
+            //     println!("offset: {offset}, n: {n}");
+            //    let end = offset +n;
+            //     // 转换指令为字符串 
+            //     if let Ok(s) = std::str::from_utf8(&buf[..end]) {
+            //         println!("Received: {}", s);
 
-                    // 处理指令
-                    let output = process(s).await;
-                    println!("Processed: {}", output);
-                    // 发送处理结果
-                    socket.write_all(&output.as_bytes()).await.expect("failed to write data to socket");
-                }else {
-                    // 判断是否转换失败，如果失败，就有可能是网络上的数据还没读完
-                   // 要继续loop读下一波数据     
-                      offset = end;
+            //         // 处理指令
+            //         let output = process(s).await;
+            //         println!("Processed: {}", output);
+            //         // 发送处理结果
+            //         socket.write_all(&output.as_bytes()).await.expect("failed to write data to socket");
+            //     }else {
+            //         // 判断是否转换失败，如果失败，就有可能是网络上的数据还没读完
+            //        // 要继续loop读下一波数据     
+            //           offset = end;
+            //     }
+            // }
+
+            // v2
+            while let Some(msg) = framed_stream.next().await {
+              match msg {
+                Ok(msg) => {
+                        let directive = String::from_utf8(msg.to_vec()).expect("invalid utf8");
+                        println!("Received: {}", directive);
+                        let output = process(&directive).await;
+                        println!("Processed: {}", output);
+                       _ = framed_stream.send(Bytes::from(output)).await;
+                    }
+                Err(e) => {
+                    println!("failed to read from socket; error = {:?}", e);
+                    return;
+                    }
                 }
             }
-            // 读取数据
-        });
+         });
     }
 }
 
